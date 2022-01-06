41157f71459b004d78c767dae18596b1eca61beb
Victor Leshkov <7065531+VLeshkov@users.noreply.github.com>
2022-01-07 01:08:58 +0600

    feat: add portfolio section

--- portfolio/index.js
+++ portfolio/index.js
@@ -16,3 +16,30 @@ switchRu.addEventListener('click', () => {
     switchEng.classList.remove('switch-selected');
   }
 });
+
+// code for portfolio buttons
+
+let portfolioButtons = document.querySelector('.portfolio-controls').
+  querySelectorAll('button');
+
+function getSelectedButton(buttonList) {
+  for (let i = 0; i < buttonList.length; i++) {
+    if (buttonList[i].classList.contains('button-checked')) {
+      return buttonList[i];
+    }
+  }
+}
+
+function changeCheckedButton(event) {
+  let clickedIndex = Array.from(portfolioButtons).indexOf(event.target);
+  let currentButton = portfolioButtons[clickedIndex];
+
+  if (!currentButton.classList.contains('button-checked')) {
+    getSelectedButton(portfolioButtons).classList.remove('button-checked');
+    currentButton.classList.add('button-checked');
+  }
+}
+
+portfolioButtons.forEach(btn => {
+  btn.addEventListener('click', changeCheckedButton);
+})
